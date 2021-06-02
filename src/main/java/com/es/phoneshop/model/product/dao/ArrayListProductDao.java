c74ecaea1fd13929a4f32d94ac630f728f52f4be
Akello32 <matmux23@gmail.com>
2021-06-02 23:42:15 +0300

    Redesigned filterProductsByQuery method

--- src/main/java/com/es/phoneshop/model/product/dao/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/dao/ArrayListProductDao.java
@@ -7,16 +7,15 @@ import com.es.phoneshop.model.product.dao.searchParam.SortParam;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
-import java.util.function.Function;
 import java.util.stream.Collectors;
-
-import static java.util.Comparator.reverseOrder;
 
 class ArrayListProductDao implements ProductDao {
     private Long maxId = 0L;
@@ -59,28 +58,35 @@ class ArrayListProductDao implements ProductDao {
     private List<Product> filterProductsByQuery(String query, SortParam param, SortOrder order) {
         Comparator<Product> comparator;
         String[] queries = query.toLowerCase().trim().split("\\s+");
-        if (param == null || !sortingFunctions.containsKey(param)) {
-            Function<Product, Long> countMatches = p -> Arrays.stream(queries)
+        Map<Product, Long> countMatchesMap = new HashMap<>();
+        List<Product> result;
+
+        products.forEach(p -> {
+            int counter = (int) Arrays.stream(queries)
                     .filter(q -> p.getDescription().toLowerCase().contains(q.trim()))
                     .count();
-            comparator = Comparator.comparing(countMatches, reverseOrder());
+            if (counter != 0) {
+                countMatchesMap.put(p, (long) counter);
+            }
+        });
+
+        if (param == null || !sortingFunctions.containsKey(param)) {
+            result = countMatchesMap.entrySet()
+                    .stream()
+                    .sorted(Map.Entry.comparingByValue())
+                    .map(Map.Entry::getKey)
+                    .collect(Collectors.toList());
+            Collections.reverse(result);
         } else {
             comparator = sortingFunctions.get(param);
             if (SortOrder.DESCEND == order) {
                 comparator = comparator.reversed();
             }
+            result = new ArrayList<>(countMatchesMap.keySet());
+            result.sort(comparator);
         }
 
-        return filterProductsByStockPrice(products.stream()
-                .filter(p -> {
-                    int counter = (int) Arrays.stream(queries)
-                            .filter(q -> p.getDescription().toLowerCase().contains(q.trim()))
-                            .count();
-                    return counter != 0;
-                })
-                .sorted(comparator)
-                .collect(Collectors.toList())
-        );
+        return filterProductsByStockPrice(result);
     }
 
     private List<Product> filterProductsByStockPrice(List<Product> productList) {
